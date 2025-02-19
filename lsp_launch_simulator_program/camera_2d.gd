b2b07bde93fa6335dc7f3add8925443d8278ff24
MrL <57445985+MrLuis5322@users.noreply.github.com>
2025-02-19 17:11:28 -0500

    Ajout limite de zoom et position
    En developpement...

--- lsp_launch_simulator_program/camera_2d.gd
+++ lsp_launch_simulator_program/camera_2d.gd
@@ -12,6 +12,16 @@ var isDragging : bool = false
 # Sensitivity for drag scaling
 @export var dragSensitivity : float = 0.5
 
+# Limits for zoom
+var minZoom : float = 0.5
+var maxZoom : float = 4
+
+# Limits for camera position  A RETRAVAILLER
+var minX : float = 1150
+var maxX : float = 2500
+var minY : float = 648
+var maxY : float = 3000
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
     zoomTarget = zoom
@@ -32,6 +42,10 @@ func Zoom(delta):
     if Input.is_action_just_pressed("camera_zoom_out"):
         zoomTarget *= 0.9
 
+    # Apply zoom limits
+    zoomTarget.x = clamp(zoomTarget.x, minZoom, maxZoom)
+    zoomTarget.y = clamp(zoomTarget.y, minZoom, maxZoom)
+
     # Smooth zoom transition
     zoom = zoom.slerp(zoomTarget, 5 * delta)
 
@@ -51,6 +65,10 @@ func SimplePan(delta):
     # Adjust movement speed based on zoom level
     position += moveAmount * delta * moveSpeed * (1 / zoom.x)
 
+    # Apply position limits
+    position.x = clamp(position.x, minX, maxX)
+    position.y = clamp(position.y, minY, maxY)
+
 func ClickAndDrag():
     if !isDragging and Input.is_action_just_pressed("camera_pan"):
         dragStartMousePosition = get_global_mouse_position()
@@ -63,4 +81,8 @@ func ClickAndDrag():
     if isDragging:
         var moveVector = get_global_mouse_position() - dragStartMousePosition
         # Apply the drag sensitivity to adjust the movement scaling
-        position = dragStartCameraPosition - moveVector * dragSensitivity *0.00001/zoom.x 
+        position = dragStartCameraPosition - moveVector * dragSensitivity * 0.00001 / zoom.x
+
+        # Apply position limits
+        position.x = clamp(position.x, minX, maxX)
+        position.y = clamp(position.y, minY, maxY)
